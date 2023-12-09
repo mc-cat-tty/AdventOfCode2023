20db1641750924ea2951980cec19bec66b79f6ce
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-12-09 11:22:51 +0100

    end day 9

---
+++ day9/oasis_instability_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+using Timeseries = vector<int>;
+
+auto prediction(const Timeseries &v) {
+  if (v.empty()) return 0;
+
+  Timeseries t;
+  bool allZero = true;
+  for (int i=0, e=v.size()-1; i<e; i++) {
+    int diff = v.at(i+1) - v.at(i);
+    t.emplace_back(diff);
+
+    if (allZero and diff != 0) allZero = false;
+  }
+
+  return *v.begin() - (allZero ? 0 : prediction(t));
+}
+
+auto sumPrediction(const vector<Timeseries> &v) {
+  return accumulate(v.begin(), v.end(), 0, [](auto acc, auto x){ return acc + prediction(x); });
+}
+
+int main() {
+  ifstream in("input.txt");
+  vector<Timeseries> v;
+  string buf;
+
+  while (getline(in, buf)) {
+    stringstream ss(buf);
+    v.emplace_back(
+      Timeseries(istream_iterator<int>(ss), istream_iterator<int>())
+    );
+  }
+
+  if (not in.flags()) {
+    cerr << "Error while reading input" << endl;
+    return 1;
+  }
+
+  in.close();
+
+  cout << sumPrediction(v) << endl;
+}
